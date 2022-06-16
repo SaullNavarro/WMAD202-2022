b7e43c713b6b171e663b6c2b3319eeb68c7dcff6
SaullNavarro <104868856+SaullNavarro@users.noreply.github.com>
2022-06-15 21:01:16 -0700

    add assignment 3, question 2

--- wmad202-assignment3/src/ca/ciccc/wmad/assignment3/main/question2/Question2.java
+++ wmad202-assignment3/src/ca/ciccc/wmad/assignment3/main/question2/Question2.java
@@ -1,10 +1,20 @@
 package ca.ciccc.wmad.assignment3.main.question2;
 
+import ca.ciccc.wmad.assignment3.main.question1.Question1;
+
 import java.util.Scanner;
 
 public class Question2 {
     public static void closestPrimeNumber(){
         Scanner in = new Scanner(System.in);
+        System.out.println("Please enter a number: ");
+        int number = in.nextInt();
+
+        int newNumber = number + 1;
+        while (!Question1.isPrimeNumber(newNumber)){
+            newNumber++;
+        }
+        System.out.printf("The closest prime number is %d,\n",newNumber);
 
     }
 }
